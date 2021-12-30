735b64cb136148970ecf1b4b7c7cc09358782b4b
Asako Kondo <kondoasako@gmail.com>
2021-12-30 15:27:35 +0900

    Boardを追加

---
+++ src/components/Board.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            my Trello
+        </header>
+        <main>
+            <p class="info-line">タスク合計数: 0</p>
+        </main>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+
+</script>
